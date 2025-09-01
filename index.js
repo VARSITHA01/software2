834b983d0ee20ca467cc41b0dff1fe9a4a2e43c8
Mangai Varsitha <mangaivarsitha@gmail.com>
2025-09-01 16:31:30 +0530

    commited

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, my Node.js app is running!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello, my Node.js app is running!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
